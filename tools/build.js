3b09393fc38f92f29be06c3e2b40517a3c8bf594
odorajbotoj <odoraj_botoj@tom.com>
2024-07-01 22:17:55 +0800

    add: build.js

---
+++ tools/build.js
@@ -0,0 +1,17 @@
+require("esbuild").buildSync({
+    entryPoints: ["scripts/main.ts"],
+    bundle: true,
+    outfile: "src_out/scripts/main.js",
+    external: [
+        "@minecraft/common",
+        "@minecraft/debug-utilities",
+        "@minecraft/server-admin",
+        "@minecraft/server-gametest",
+        "@minecraft/server-net",
+        "@minecraft/server-ui",
+        "@minecraft/server",
+        "@minecraft/server-editor"
+    ],
+    sourcemap: false,
+    minify: true
+});
